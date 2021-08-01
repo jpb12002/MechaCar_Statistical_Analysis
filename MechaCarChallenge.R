02dc567056cdee541a211322728e611f77544f06
Jordan Bellucci <jordan.bellucci94@gmail.com>
2021-08-01 18:57:21 -0500

    Adding challenge files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,15 @@ total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI
 
 # Create "lot_summary" dataframe
 lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# t-test for all manufacturing lots
+t.test(Suspension_Coil$PSI,mu=1500)
+
+Lot1 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot1')
+Lot2 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot2')
+Lot3 <- subset(Suspension_Coil, Manufacturing_Lot == 'Lot3')
+
+# t-test for individual manufacturing lots
+t.test(Lot1$PSI,mu=1500)
+t.test(Lot2$PSI,mu=1500)
+t.test(Lot3$PSI,mu=1500)
