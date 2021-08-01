e1088f26ae3f48b3dbb272064e9a2222a867ec68
Jordan Bellucci <jordan.bellucci94@gmail.com>
2021-08-01 17:31:06 -0500

    Updating challenge files

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,15 @@ mpg_regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + gro
 
 # Summarize linear model
 summary(mpg_regression)
+
+# Import and Read CSV file as dataframe
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Import Library
+library(tidyverse)
+
+# Create "total_summary" dataframe
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) 
+
+# Create "lot_summary" dataframe
+lot_summary <- Suspension_Coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
